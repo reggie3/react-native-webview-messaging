a089281fa0f8fa087b7e624176465e89e9deb695
Reginald Johnson <reginald.johnson@gmail.com>
2017-08-22 18:41:24 -0400

    confirming alerts work

--- examples/expo/web/index.js
+++ examples/expo/web/index.js
@@ -1,34 +1,44 @@
-import RNMessageChannel from 'react-native-webview-messaging';
+import RNMessageChannel from "react-native-webview-messaging";
 
-const helloBtn = document.querySelector('#hello');
-const jsonBtn = document.querySelector('#json');
-const eventBtn = document.querySelector('#event');
-const messagesContainer = document.querySelector('p');
+const helloBtn = document.querySelector("#hello");
+const jsonBtn = document.querySelector("#json");
+const eventBtn = document.querySelector("#event");
+const messagesContainer = document.querySelector("p");
 
-helloBtn.addEventListener('click', () => {
-  RNMessageChannel.send('hello');
+helloBtn.addEventListener("click", () => {
+  RNMessageChannel.send("hello");
 });
 
-jsonBtn.addEventListener('click', () => {
+jsonBtn.addEventListener("click", () => {
   RNMessageChannel.sendJSON({
-    payload: 'hello'
+    payload: "hello"
   });
 });
 
-eventBtn.addEventListener('click', () => {
-  RNMessageChannel.emit('greetingFromWebview', {
-    payload: 'hello'
+eventBtn.addEventListener("click", () => {
+  RNMessageChannel.emit("greetingFromWebview", {
+    payload: "hello"
   });
 });
 
-RNMessageChannel.on('text', text => {
+RNMessageChannel.on("text", text => {
+  alert(text);
+  console.log(text);
   messagesContainer.innerHTML = `Received text from RN: ${text}`;
 });
 
-RNMessageChannel.on('json', text => {
-  messagesContainer.innerHTML = `Received json from RN: ${JSON.stringify(text)}`;
+RNMessageChannel.on("json", text => {
+  alert(text);
+  console.log(text);
+  messagesContainer.innerHTML = `Received json from RN: ${JSON.stringify(
+    text
+  )}`;
 });
 
-RNMessageChannel.on('greetingFromRN', event => {
-  messagesContainer.innerHTML = `Received "greetingFromRN" event: ${JSON.stringify(event)}`;
+RNMessageChannel.on("greetingFromRN", event => {
+  alert(event);
+  console.log(event);
+  messagesContainer.innerHTML = `Received "greetingFromRN" event: ${JSON.stringify(
+    event
+  )}`;
 });
